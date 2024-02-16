5a6c7f7377a78452a8c2003168fc4093f372d8e7
Hemant-Kshirsagar22 <hemantkshirsagar23@gmail.com>
2024-02-16 14:41:14 +0530

    Assignment

--- 06-Assignment-SerializationAndDeserialization/src/com/hgk/SERToCSV.java
+++ 06-Assignment-SerializationAndDeserialization/src/com/hgk/SERToCSV.java
@@ -1,7 +1,9 @@
 package com.hgk;
 
+import java.io.EOFException;
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
+import java.io.RandomAccessFile;
 
 import com.hgk.entities.Student;
 
@@ -13,28 +15,39 @@ public class SERToCSV
 		FileInputStream fis = null;
 		ObjectInputStream ois = null;
 		
-			
+		RandomAccessFile fw = null;
 		try
 		{
 			fis = new FileInputStream("resources/student.ser");
 			ois = new ObjectInputStream(fis);
 			
+			fw = new RandomAccessFile("resources/studentOne.csv","rw");
+			
 			Student s = null;
 			
 			while((s = (Student)ois.readObject()) != null)
 			{				
+				String line = s.getRno() + "," + s.getName() + "," + s.getPer()	+ "\n";
 				
-				System.out.println(s);
+				fw.writeBytes(line);
 			}
 		}
 		catch(Exception e)
 		{
-			e.printStackTrace();
+			if(e instanceof EOFException)
+			{
+				System.out.println("Data Decentralized Successfully !!!");
+			}
+			else
+			{
+				e.printStackTrace();
+			}
 		}
 		finally
 		{
 			try
 			{
+				fw.close();
 				ois.close();
 			}
 			catch(Exception e)
